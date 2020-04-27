61ef7a776b9aaf002fde00adde7f6ce223d992cb
adevr <alex.cst.reis@gmail.com>
2020-04-27 23:14:19 +0100

    feat: register SpaceShips and Events

---
+++ src/logica/dados/GameData.java
@@ -0,0 +1,40 @@
+package logica.dados;
+
+import java.util.List;
+
+import logica.dados.evento.*;
+import logica.dados.nave.*;
+
+public class GameData
+{
+
+    List<Event> events;
+    List<SpaceShip> ships;
+    SpaceShip selectedShip;
+
+    public GameData()
+    {
+        this.registerEvents();
+    }
+
+    private void registerEvents()
+    {
+        events.add(new AsteroidEvent(selectedShip));
+        events.add(new CrewDeathEvent(selectedShip));
+        events.add(new SalvageShipEvent(selectedShip));
+        events.add(new CargoLossEvent(selectedShip));
+        events.add(new FuelLossEvent(selectedShip));
+        events.add(new NoneEvent(selectedShip));
+        events.add(new AlienAttackEvent(selectedShip));
+        events.add(new CrewRescueEvent(selectedShip));
+        events.add(new LostEvent(selectedShip));
+        events.add(new StrongAlienAttack(selectedShip));
+    }
+
+    private void registerShips()
+    {
+        ships.add(new MilitaryShip());
+        ships.add(new MiningShip());
+    }
+
+}
