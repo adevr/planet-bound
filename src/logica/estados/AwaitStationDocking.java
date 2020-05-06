649e9d443536ff7db547b482708cb9594febeae1
adevr <alex.cst.reis@gmail.com>
2020-05-06 23:41:23 +0100

    feat: new AwaitStationDocking state

---
+++ src/logica/estados/AwaitStationDocking.java
@@ -0,0 +1,11 @@
+package logica.estados;
+
+import logica.dados.GameData;
+
+public class AwaitStationDocking extends EstadoAdapter{
+    public AwaitStationDocking(GameData data)
+    {
+        super(data);
+    }
+
+}
