1bca66f7f0be5c895528c22634d587c55e381f3d
adevr <alex.cst.reis@gmail.com>
2020-05-06 23:40:59 +0100

    chore: create resource class

---
+++ src/logica/dados/Resource.java
@@ -0,0 +1,16 @@
+package logica.dados;
+
+public class Resource {
+
+    private String nome;
+
+    public Resource(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
