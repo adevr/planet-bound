531fea130ef20ca102c8c9a33cabb77289fd4455
adevr <alex.cst.reis@gmail.com>
2020-06-09 22:22:00 +0100

    refactor: new main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,21 @@
 package sample;
 
-import logica.StateMachine;
+import logic.StateMachine;
 import ui.text.UIText;
 
 public class Main {
+/*
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }*/
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args)
+    {
         UIText ui = new UIText(new StateMachine());
         ui.execute();
     }
